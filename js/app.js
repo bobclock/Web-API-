7402f027055110dfc7672e88e6ff8db2e3de746f
bobclock <bmb5rjc@gmail.com>
2016-09-05 21:43:24 +0100

    First upload

---
+++ js/app.js
@@ -0,0 +1,18 @@
+// JQUERY VARIABLES
+
+$hourly = $("#hourly");
+$daily = $("#daily");
+$weekly = $("#weekly");
+$monthly = $("#monthly");
+$traffic_chart_hourly = $("#traffic_chart_hourly");
+$traffic_chart_daily = $("#traffic_chart_daily");
+$traffic_chart_daily2 = $("#traffic_chart_daily2");
+$traffic_chart_weekly = $("#traffic_chart_weekly");
+$traffic_chart_monthly = $("#traffic_chart_monthly");
+$mobile_users_chart = $("#mobile_users_chart");
+$notifications_txt = $("#notifications_txt");
+$notifications_bell = $("#bell_img");
+$alert_div = $(".alert");
+$close_alert = $(".close");
+$save = $("#save_settings");
+$cancel = $("#cancel_settings");
